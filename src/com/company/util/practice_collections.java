8ef167f8e0940cbd015901180257498ebb727cd1
kabir5145 <kabir10112006@gmail.com>
2025-08-07 18:14:12 +0530

    Question 1 of arraylist

---
+++ src/com/company/util/practice_collections.java
@@ -0,0 +1,15 @@
+package com.company.util;
+
+import java.util.ArrayList;
+
+public class practice_collections {
+    public static void main(String[] args) {
+        //question 1: write a java program to create an array list , add some colors (String) and print out the collections
+        ArrayList<String>  colors = new ArrayList<>();
+        colors.add("red");
+        colors.add("blue");
+        colors.add("yellow");
+        colors.add("green");
+        System.out.println(colors);
+    }
+}
