200200576c89b6f544ce4490cc60878d0fdf4eb1
kabir5145 <kabir10112006@gmail.com>
2025-08-19 01:39:28 +0530

    question 10 of arraylist

--- src/com/company/util/practice_collections.java
+++ src/com/company/util/practice_collections.java
@@ -4,143 +4,17 @@ import java.util.*;
 
 public class practice_collections {
     public static void main(String[] args) {
-        //question 1: write a java program to create an array list , add some colors (String) and print out the collections
-        /*
-        ArrayList<String>  colors = new ArrayList<>();
-        colors.add("red");
-        colors.add("blue");
-        colors.add("yellow");
-        colors.add("green");
-        System.out.println(colors);
+      //question 10: Shuffle Arraylist
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(6);
 
-        //question 2: Write a java program to iterate through all elements in an array list.
-        ArrayList<String>  color = new ArrayList<>();
-        color.add("red");
-        color.add("blue");
-        color.add("yellow");
-        color.add("green");
-      for(String element: color){
-          System.out.println(element);
-      }
-         */
-
-        //question 3 : write a java program to insert an element into the array list at the first position
-        /*ArrayList<String> name = new ArrayList<>();
-        name.add("kabir");
-        name.add("Batman");
-        name.add("Messi");
-        name.add("Ronaldo");
-        name.add(1,"hello");
-        System.out.println(name);
-        for(String element: name) {
-            System.out.println(element);
-        }
-         */
-
-        //question 4 : Write a java program to retrieve an element (at a specific index) from a given array list.
-        /*ArrayList<Integer> num= new ArrayList<>();
-        num.add(1);
-        num.add(12);
-        num.add(123);
-        num.add(1234);
-        num.add(12345);
-        for(Integer element :num) {
-            System.out.println(element);
-        }
-        System.out.println("First element is : "+num.get(0));
-        System.out.println("Second element is : "+num.get(1));
-         */
-
-        //question 5: Write a java program to update an array element by given element.
-        /*
-        ArrayList<String > color = new ArrayList<>();
-        color.add("red");
-        color.add("blue");
-        color.add("yellow");
-        color.add("green");
-        System.out.println(color);
-        color.set(2,"Black");
-        System.out.println(color);
-         */
-
-        //question 6: Write a java program to remove the third element from an arraylist
-        /*
-        ArrayList<String> n =  new ArrayList<>();
-        n.add("kabir");
-        n.add("Rashi");
-        n.add("Rehan");
-        n.add("Robin");
-        n.add("Rishi");
-        System.out.println("Original array is : "+n);
-        System.out.println(n.remove(3));
-        System.out.println("Now the array is : "+n);
-         */
-
-        //Question 7 : Write a java program to search for an element in an array list
-        /*
-        ArrayList<Integer> num = new ArrayList<>();
-        num.add(12);
-        num.add(13);
-        num.add(14);
-        num.add(15);
-        num.add(16);
-        num.add(17);
-        num.add(18);
-        num.add(19);
-        num.add(20);
-        System.out.println(num);
-
-        if(num.contains(12)){
-            System.out.println("found the number !");
-        }else {
-            System.out.println("not found the number!");
-        }
-
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Insert a number : ");
-       if(num.contains(sc.nextInt())){
-           System.out.println("yes found the number !");
-       }else {
-           System.out.println("Not found the number!");
-       }
-         */
-
-        //Question 8 : Write a java program to sort a given array list
-        /*
-        ArrayList<Integer> num = new ArrayList<>();
-        num.add(122);
-        num.add(13);
-        num.add(14);
-        num.add(15);
-        num.add(16);
-        num.add(17);
-        num.add(18);
-        num.add(19);
-        for (int i : num)
-            System.out.print(i+" ");
-        System.out.println();
-        Collections.sort(num);
-        System.out.print("List after sort is : "+num);
-         */
-
-        //Question 9: Write a java program to copy one array list into another
-        LinkedList<Integer> num = new LinkedList<>();
-        num.add(1);
-        num.add(2);
-        num.add(3);
-        num.add(4);
-        num.add(5);
-        num.add(6);
-        System.out.println("The original  first array is : "+num);
-        LinkedList<Integer> num2 = new LinkedList<>();
-        num2.add(11);
-        num2.add(12);
-        num2.add(13);
-        num2.add(14);
-        num2.add(15);
-        num2.add(16);
-        System.out.println("The second array is : "+num2);
-        Collections.copy(num,num2);
-        System.out.println("The new copied array is : "+num);
+        System.out.println("Numbers before shuffling : "+numbers);
+        Collections.shuffle(numbers);
+        System.out.println("Numbers after shuffling : "+numbers);
     }
 }
