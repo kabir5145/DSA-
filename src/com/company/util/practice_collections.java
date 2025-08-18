1fd7ca5393e005128ee2da4588509f106b35c551
kabir5145 <kabir10112006@gmail.com>
2025-08-19 01:43:54 +0530

    question 11 of arraylist

--- src/com/company/util/practice_collections.java
+++ src/com/company/util/practice_collections.java
@@ -4,17 +4,14 @@ import java.util.*;
 
 public class practice_collections {
     public static void main(String[] args) {
-      //question 10: Shuffle Arraylist
-        ArrayList<Integer> numbers = new ArrayList<>();
-        numbers.add(1);
-        numbers.add(2);
-        numbers.add(3);
-        numbers.add(4);
-        numbers.add(5);
-        numbers.add(6);
-
-        System.out.println("Numbers before shuffling : "+numbers);
-        Collections.shuffle(numbers);
-        System.out.println("Numbers after shuffling : "+numbers);
+      //question 11: Reverse Arraylist
+        ArrayList<Integer> n = new ArrayList<>();
+        n.add(12);
+        n.add(36);
+        n.add(98);
+        n.add(43);
+        System.out.println("The original array is : "+n);
+        Collections.reverse(n);
+        System.out.println("The reversed array is : "+n);
     }
 }
