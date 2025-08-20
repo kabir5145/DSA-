8ef081d23e00e7fc139a7173053531981124c1b5
kabir5145 <kabir10112006@gmail.com>
2025-08-20 22:48:01 +0530

    selection sort in java

--- src/com/company/sorting.java
+++ src/com/company/sorting.java
@@ -13,16 +13,17 @@ public class sorting {
         int arr[] = {7,8,3,1,2};
 
         //time complexity = 0(n^2)
-        //bubble sort
-        for(int i = 0;i<arr.length;i++){//n-1
-           for(int j = 0;j<arr.length-i-1;j++){
-               if(arr[j]>arr[j+1]){
-                   //swap
-                   int temp = arr[j];
-                   arr[j] = arr[j+1];
-                   arr[j+1]=temp;
-               }
-           }
+       //selection sort
+        for(int i = 0;i<arr.length-1;i++){
+            int smallest = i;
+            for(int j = i+1;j<arr.length;j++){
+                if(arr[smallest]>arr[j]){
+                    smallest = j;
+                }
+            }
+            int temp = arr[smallest];
+            arr[smallest]=arr[i];
+            arr[i]=temp;
         }
         printArray(arr);
     }
