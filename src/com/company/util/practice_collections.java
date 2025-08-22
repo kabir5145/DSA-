0ad6f43a4b0dd8352eedb934fca2d36e0cfbdece
kabir5145 <kabir10112006@gmail.com>
2025-08-22 23:39:33 +0530

    Question 12 of arraylist

--- src/com/company/util/practice_collections.java
+++ src/com/company/util/practice_collections.java
@@ -4,14 +4,16 @@ import java.util.*;
 
 public class practice_collections {
     public static void main(String[] args) {
-      //question 11: Reverse Arraylist
-        ArrayList<Integer> n = new ArrayList<>();
+      //question 12: Extract Sublist from ArrayList
+        List<Integer> n = new ArrayList<>();
         n.add(12);
-        n.add(36);
-        n.add(98);
-        n.add(43);
-        System.out.println("The original array is : "+n);
-        Collections.reverse(n);
-        System.out.println("The reversed array is : "+n);
+        n.add(13);
+        n.add(14);
+        n.add(15);
+        n.add(16);
+        n.add(17);
+        System.out.println("Original List is : "+n);
+        List<Integer> v = n.subList(0,3);
+        System.out.println("List of three elements is : "+v);
     }
 }
