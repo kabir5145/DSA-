500dbfe0e5fd67b71b836df04ec3bb3ee7899810
kabir5145 <kabir10112006@gmail.com>
2025-08-22 23:57:26 +0530

    Question 13 of arraylist

--- src/com/company/util/practice_collections.java
+++ src/com/company/util/practice_collections.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class practice_collections {
     public static void main(String[] args) {
       //question 12: Extract Sublist from ArrayList
+        /*
         List<Integer> n = new ArrayList<>();
         n.add(12);
         n.add(13);
@@ -15,5 +16,22 @@ public class practice_collections {
         System.out.println("Original List is : "+n);
         List<Integer> v = n.subList(0,3);
         System.out.println("List of three elements is : "+v);
+         */
+
+        //question 13: Compare Two ArrayList
+        List<Integer> c = new ArrayList<>();
+        c.add(1);
+        c.add(2);
+        c.add(3);
+        System.out.println("The first original list is : "+c);
+        List<Integer> b = new ArrayList<>();
+        b.add(4);
+        b.add(5);
+        b.add(6);
+        System.out.println("The second original list is :"+b);
+        List<Integer> combine = new ArrayList<>();
+        combine.addAll(c);
+        combine.addAll(b);
+        System.out.println("The combined list is : "+combine);
     }
 }
