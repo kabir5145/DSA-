15fd18501a55444d22476ec05d8f3192fd671f23
kabir5145 <kabir10112006@gmail.com>
2025-09-13 17:05:28 +0530

    Time complexity example 2 for linear algorithm

--- src/com/company/T_C_Constant_Algo.java
+++ src/com/company/T_C_Constant_Algo.java
@@ -6,6 +6,13 @@ public class T_C_Constant_Algo {
         int result = x+y;
         return result;
     }
+    public static int list_Sum(int []A,int n){
+        int sum = 0;
+        for(int i = 0 ;i<=n-1;i++){
+            sum = sum +A[i];
+        }
+        return sum;
+    }
     public static void main(String[] args) {
         //measure runtime in java
         long start =System.nanoTime();
@@ -15,5 +22,9 @@ public class T_C_Constant_Algo {
         long end = System.nanoTime();    // end time
         System.out.println("The sum of two numbers is: " + res);
         System.out.println("Time taken (ns): " + (end - start));
+
+        int []A = {5,6,1,2};
+        int n = A.length;
+        System.out.println(list_Sum(A,n));
     }
 }
